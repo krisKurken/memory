4f822132607c22765d57a2ad8beacd67ec968999
krisKurken <kristian.liljander@live.se>
2017-12-09 14:39:12 +0100

    Start button && counter

--- functions/functions.js
+++ functions/functions.js
@@ -1,4 +1,11 @@
 "use strict"
+
+function StartBtn(){
+  const btn = document.querySelector(".startBtn");
+  btn.addEventListener("click", InitGame);
+  btn.addEventListener("click", function(){btn.remove();});
+}
+
 //Initierar spelet
 function InitGame(){
   const cards = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8];
@@ -6,7 +13,17 @@ function InitGame(){
     InitCard(cards.splice(Math.floor(Math.random()*cards.length), 1));
   }
   document.querySelector(".button").addEventListener("click", InitNewGame);
+  document.querySelector(".board").dataset.moves = 0;
+  ShowGame();
 }
+
+function ShowGame(){
+  setTimeout(function(){
+    document.querySelector(".board").style.opacity = "1";
+    document.querySelector(".button").style.opacity = "0.89";
+  }, 300);
+}
+
 //Initierar varje kort
 function InitCard(randomCard){
   const card = document.createElement("div");
@@ -15,35 +32,34 @@ function InitCard(randomCard){
   card.dataset.active = 0;
   card.dataset.flipped = 0;
   card.addEventListener("click", HandleCard);
-  document.querySelector(".frame").append(card);
+  document.querySelector(".board").append(card);
 }
-// //Initierar ett nytt spel via en en knapp
-// function ReplayBtn(){
-//   document.querySelector(".button").addEventListener("click", InitNewGame);
-// }
+
 //Tar bort föregående bräde och startar ett nytt spel
 function InitNewGame(){
-  const frame = document.querySelector(".frame");
-  while (frame.firstChild) {
-    frame.removeChild(frame.firstChild);
+  const board = document.querySelector(".board");
+  while (board.firstChild) {
+    board.removeChild(board.firstChild);
   }
   return InitGame();
 }
 //Hanterar vad som händer när man klickar på ett kort
 function HandleCard(){
-  if (IsNotPair()) {
+  const board = document.querySelector(".board");
+  if (IsPair()) {
     UnflipCards();
   }
   FlipCard(this);
+  board.dataset.moves = parseInt(board.dataset.moves) + 1;
 }
 
 //kollar om det är ett par bland de flippade korten
-function IsNotPair(){
-  const frame = document.querySelector(".frame").childNodes;
+function IsPair(){
+  const board = document.querySelector(".board").childNodes;
   var temp = [];
-  for (var i = 0; i < frame.length; i++) {
-    if (frame[i].dataset.flipped === "1") {
-      temp.push(frame[i]);
+  for (var i = 0; i < board.length; i++) {
+    if (board[i].dataset.flipped === "1") {
+      temp.push(board[i]);
       if (temp.length >=2 && temp[0].dataset.card === temp[1].dataset.card) {
         HandlePair(temp[0], temp[1]);
         return false;
@@ -71,9 +87,9 @@ function FlipCard(card){
 
 //Kollar om spelet är klart
 function IsGameDone(){
-  const frame = document.querySelector(".frame").childNodes;
-  for (var i = 0; i < frame.length; i++) {
-    if (frame[i].getAttribute("data-active") === "0") {
+  const board = document.querySelector(".board").childNodes;
+  for (var i = 0; i < board.length; i++) {
+    if (board[i].getAttribute("data-active") === "0") {
       return false;
     }
   }
@@ -82,7 +98,7 @@ function IsGameDone(){
 
 //Flippar tillbaka alla flippade kort på brädet
 function UnflipCards(){
-  const cards = document.querySelector(".frame").childNodes;
+  const cards = document.querySelector(".board").childNodes;
   for (var i = 0; i < cards.length; i++) {
     if (cards[i].dataset.flipped === "1") {
       cards[i].dataset.active = 0;
