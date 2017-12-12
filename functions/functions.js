1827d6e2baeab1aa45f8a0af5591871976064be9
krisKurken <kristian.liljander@live.se>
2017-12-12 22:35:48 +0100

    Edit

--- functions/functions.js
+++ functions/functions.js
@@ -1,10 +1,10 @@
 "use strict"
 
 /**
- * StartBtn - Handles functionality for the startbutton
- * and removes it after its been "clicked".
- * @return {none}
- */
+* StartBtn - Handles functionality for the startbutton
+* and removes it after its been "clicked".
+* @return {none}
+*/
 function StartBtn(){
   const btn = document.querySelector(".startBtn");
   btn.addEventListener("click", InitGame);
@@ -12,10 +12,10 @@ function StartBtn(){
 }
 
 /**
- * InitGame - Initializes the game by calling all necessary
- * functions.
- * @return {none}
- */
+* InitGame - Initializes the game by calling all necessary
+* functions.
+* @return {none}
+*/
 function InitGame(){
   const cards = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8];
   CreateBoard();
@@ -28,10 +28,10 @@ function InitGame(){
 }
 
 /**
- * CreateBoard - Creates the board and replay button and
- * appends it to the wrapper div.
- * @return {none}
- */
+* CreateBoard - Creates the board and replay button and
+* appends it to the wrapper div.
+* @return {none}
+*/
 function CreateBoard(){
   const wrapper = document.querySelector(".wrapper");
   const board = document.createElement("div");
@@ -46,10 +46,10 @@ function CreateBoard(){
 }
 
 /**
- * ShowGame - Creates the transition effect for the game board
- * and the replay button.
- * @return {none}
- */
+* ShowGame - Creates the transition effect for the game board
+* and the replay button.
+* @return {none}
+*/
 function ShowGame(){
   setTimeout(function(){
     document.querySelector(".board").style.opacity = "1";
@@ -58,11 +58,11 @@ function ShowGame(){
 }
 
 /**
- * InitCard - Initializes every card, appends it to the
- * board and adds a click event to it.
- * @param  {int} randomCard - Random int that represents the card.
- * @return {none}
- */
+* InitCard - Initializes every card, appends it to the
+* board and adds a click event to it.
+* @param  {int} randomCard - Random int that represents the card.
+* @return {none}
+*/
 function InitCard(randomCard){
   const card = document.createElement("div");
   card.setAttribute("class", "card");
@@ -73,12 +73,11 @@ function InitCard(randomCard){
   document.querySelector(".board").append(card);
 }
 
-
 /**
- * InitNewGame - Removes previous game if needed and
- *  calls InitGame() to start a new one.
- * @return {none}
- */
+* InitNewGame - Removes previous game if needed and
+*  calls InitGame() to start a new one.
+* @return {none}
+*/
 function InitNewGame(){
   const board = document.querySelector(".board");
   const button = document.querySelector(".button");
@@ -94,10 +93,10 @@ function InitNewGame(){
 }
 
 /**
- * HandleCard - Handles click events for all the cards and calls the
- * appropriate function. It also adds to the amount of moves variable.
- * @return {none}
- */
+* HandleCard - Handles click events for all the cards and calls the
+* appropriate function. It also adds to the amount of moves variable.
+* @return {none}
+*/
 function HandleCard(){
   const board = document.querySelector(".board");
   board.dataset.moves = parseInt(board.dataset.moves) + 1;
@@ -111,10 +110,10 @@ function HandleCard(){
 }
 
 /**
- * IsPair - Checks if there are two cards "flipped" and if they are
- * a pair.
- * @return {bool} - False if the cards should be flipped back.
- */
+* IsPair - Checks if there are two cards "flipped" and if they are
+* a pair.
+* @return {bool} - False if the cards should be flipped back.
+*/
 function IsPair(){
   const board = document.querySelector(".board").childNodes;
   var temp = [];
@@ -134,12 +133,12 @@ function IsPair(){
 }
 
 /**
- * HandlePair - Removes the flipped status and the EventListener
- * for the cards sent to the function.
- * @param  {html object} card1 - One of the paired cards.
- * @param  {html object} card2 - One of the paired cards.
- * @return {none}
- */
+* HandlePair - Removes the flipped status and the EventListener
+* for the cards sent to the function.
+* @param  {html object} card1 - One of the paired cards.
+* @param  {html object} card2 - One of the paired cards.
+* @return {none}
+*/
 function HandlePair(card1, card2){
   card1.dataset.flipped = 0;
   card2.dataset.flipped = 0;
@@ -148,20 +147,20 @@ function HandlePair(card1, card2){
 }
 
 /**
- * FlipCard - Showes the cards and adds a flipped and active status to them.
- * @param  {html object} card - Card that will be "flipped".
- * @return {none}
- */
+* FlipCard - Showes the cards and adds a flipped and active status to them.
+* @param  {html object} card - Card that will be "flipped".
+* @return {none}
+*/
 function FlipCard(card){
   card.dataset.active = 1;
   card.dataset.flipped = 1;
 }
 
 /**
- * IsGameDone - Checks if the card "clicked" is the last
- * card in the game.
- * @return {bool} - Returns true if there is only one card left.
- */
+* IsGameDone - Checks if the card "clicked" is the last
+* card in the game.
+* @return {bool} - Returns true if there is only one card left.
+*/
 function IsGameDone(){
   const board = document.querySelector(".board").childNodes;
   var counter = 0;
@@ -177,10 +176,10 @@ function IsGameDone(){
 }
 
 /**
- * UnflipCards - Unflippes all the cards in the game with
- * flipped status 1.
- * @return {none}
- */
+* UnflipCards - Unflippes all the cards in the game with
+* flipped status 1.
+* @return {none}
+*/
 function UnflipCards(){
   const cards = document.querySelector(".board").childNodes;
   for (var i = 0; i < cards.length; i++) {
@@ -192,11 +191,11 @@ function UnflipCards(){
 }
 
 /**
- * GameDoneHandler - Creates the "gameDoneMenu" and adds it as a
- * child element to the wrapper div. The function also removes
- * previus board.
- * @return {none}
- */
+* GameDoneHandler - Creates the "gameDoneMenu" and adds it as a
+* child element to the wrapper div. The function also removes
+* previus board.
+* @return {none}
+*/
 function GameDoneHandler(){
   const gameDoneMenu = document.createElement("div");
   const menuText = document.createElement("p");
